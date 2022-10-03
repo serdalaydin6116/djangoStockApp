7ea98ed30dc93d22ac9a760ac90d7c7c08005c87
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-04 01:09:20 +0300

    app was added to installed

--- main/settings.py
+++ main/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     # My Apps
     'users',
-    'firm',
+    'info',
 ]
 
 MIDDLEWARE = [
