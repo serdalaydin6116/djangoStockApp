54b75a768d2e1d7b448421ae985ea669d5724135
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-04 01:09:34 +0300

    models were created

--- users/models.py
+++ users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class User(models.Model):
+class Auth_User(models.Model):
     
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     username=models.CharField(max_length=20)
     email=models.EmailField()
     first_name=models.CharField(max_length=20)
@@ -15,7 +15,7 @@ class User(models.Model):
     last_login= models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.username
+         return f"{self.first_name} - {self.last_name}"
 
     # class Meta:
     #     verbose_name_plural = "Users"
