967ac09f7f7b22ceda0f04502cf26cf7f35effcb
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-03 21:50:41 +0300

    new users app was created

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class User(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username=models.CharField(max_length=20)
+    email=models.EmailField()
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    is_superuser = models.BooleanField(default=False)
+    is_staff= models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+    # class Meta:
+    #     verbose_name_plural = "Users"
+
