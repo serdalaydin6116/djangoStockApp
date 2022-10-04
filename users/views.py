3e698c857631535021cceecc4ffd7503512f628f
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-05 01:24:59 +0300

    register, login and logout views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, status
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from .serializers import RegisterSerializer
+
+
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=RegisterSerializer
+
+
+    def create(self, request, *args, **kwargs):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.save()  ##BU AŞAMADA TOKEN OLUŞUR...
+        data=serializer.data
+        token=Token.objects.get(user=user)
+        data['token']=token.key
+        headers=self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
