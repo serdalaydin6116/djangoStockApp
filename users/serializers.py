7a39b28d1653358862731f8b3c0ab220a067ba2e
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-04 23:42:38 +0300

    register serializer was created

--- users/serializers.py
+++ users/serializers.py
@@ -3,3 +3,50 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth.password_validation import validate_password
 from dj_rest_auth.serializers import TokenSerializer
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[validators.UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"}
+
+    )
+
+    password1 = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"}
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'password1'
+        )
+
+    def validate(self, data):
+        if data['password'] != data['password1']:
+            raise serializers.ValidationError(
+                {"password": "Password didn't match..... "}
+            )
+        return data
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data.pop('password1')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
