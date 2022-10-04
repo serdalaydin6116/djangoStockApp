70b0346aebce41c8880c6653e2eb89806fd39fdd
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-05 01:24:08 +0300

    synched with apps

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self): 
+        import users.signals
