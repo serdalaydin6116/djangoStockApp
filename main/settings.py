37ea0befb6764489275b83e72182f69d2a2eb983
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-05 23:30:07 +0300

    pagination settings were added

--- main/settings.py
+++ main/settings.py
@@ -140,7 +140,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
+
+
 
 REST_AUTH_SERIALIZERS={
 
