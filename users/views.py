46413ba36a366d1fa5609ae39fe24010952bfdab
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-06 01:05:45 +0300

    views and generics were imported from drf

--- users/views.py
+++ users/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
+from rest_framework.views import APIView 
+from rest_framework.generics import GenericAPIView,mixins,ListCreateAPIView,RetrieveUpdateDestroyAPIView
+
 
 from .serializers import RegisterSerializer
 
